ddf561d4b2e6e2d70cc5599ce866bf30844d588a
Serge Ilinyh <serge.ilinyh@wari.com>
2017-11-03 22:01:37 +0700

    TLR-34 api generating

--- swagger-codegen/run.sh
+++ swagger-codegen/run.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-#java -jar swagger-codegen-cli-3.0.0-20171009.075709-6.jar
-#java -jar swagger-codegen-cli-3.0.0-20171009.075709-6.jar config-help -l typescript-angular2
-
-java -jar swagger-codegen-cli-3.0.0-20171009.075709-6.jar generate \
-  -i openapi.json \
-  -l typescript-angular \
+java -jar swagger-codegen-cli-2.2.3.jar generate \
+  -i http://stage.i.wari.com/teller_api/v1/swagger.json \
+  -l typescript-angular2 \
   -c config.json \
-  -o res \
-  --ignore-file-override .swagger-codegen-ignore \
+  -o ./../src/app/api \
+  --ignore-file-override /.swagger-codegen-ignore \
   -v
-
