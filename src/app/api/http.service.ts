520568721547a573feb59c89feb29e8568c8d8fc
Serge Ilinyh <serge.ilinyh@wari.com>
2017-11-22 17:52:01 +0700

    TLR-35 Handle unauthorized response 401

--- src/app/api/http.service.ts
+++ src/app/api/http.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {map} from 'rxjs/operators';
 import {
@@ -13,13 +14,22 @@ import {AuthService} from '../services/auth.service';
 export class HttpService {
 
   constructor(private http: Http,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private router: Router) {
   }
 
   request(url: string, options?: RequestOptionsArgs): Observable<Response> {
-    // todo handle interceptions here
     const options1 = this.addAuthorization(options);
-    return this.http.request(url, options1).pipe(
+    const observable: Observable<Response> = this.http.request(url, options1);
+
+    observable.subscribe(null, (response: Response) => {
+      if (response.status === 401) {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      }
+    });
+
+    return observable.pipe(
       map((response: Response): Response => {
         const responseAuthorization = response.headers.get('Authorization');
         if (responseAuthorization) {
