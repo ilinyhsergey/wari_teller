df7f75f5e29a799c11fc5ce8b26e2793c64cacf7
Serge Ilinyh <serge.ilinyh@wari.com>
2017-12-05 18:47:10 +0700

    TLR-148 fix caching process

--- src/app/services/base-cache.service.ts
+++ src/app/services/base-cache.service.ts
@@ -5,6 +5,8 @@ import * as _ from 'lodash';
 
 import {environment} from '../../environments/environment';
 import {StorageService} from './storage.service';
+import {TeardownLogic} from 'rxjs/src/Subscription';
+import {Subscriber} from 'rxjs/src/Subscriber';
 
 @Injectable()
 export class BaseCacheService {
@@ -16,32 +18,42 @@ export class BaseCacheService {
   }
 
   protected getExpiredCacheValue<T>(key: string, load: () => Observable<T>): Observable<T> {
-    const itemValue: T = <T>this.storageService.get(key);
-    if (itemValue) {
-      // already cached
-      this.memMap[key] = itemValue; // update if app just started
-      return Observable.of(_.cloneDeep(itemValue)); // return immediately
-    } else {
-      // if doesn't exist or expired
-      const loadedOb = load();
-      loadedOb.subscribe((loadedItemValue: T) => {
+    return Observable.create((subscriber: Subscriber<T>): TeardownLogic => {
+
+      const itemValue: T = <T>this.storageService.get(key);
+      if (itemValue) {
+
+        // already cached
+        this.memMap[key] = itemValue; // update if app just started
+        subscriber.next(_.cloneDeep(itemValue));
+        subscriber.complete();
+
+      } else {
+
+        const memValue: T = <T>this.memMap[key];
+        if (memValue) {
+          // return immediately
+          subscriber.next(_.cloneDeep(memValue));
+          subscriber.complete();
+        }
+
+        // if doesn't exist or expired
+        load().subscribe((loadedItemValue: T) => {
           // update in background
           if (loadedItemValue) {
             this.storageService.set(key, loadedItemValue, this.expirationMinutesMap[key]);
+            subscriber.next(loadedItemValue);
+            subscriber.complete();
           }
-        },
-        // Need this to propagate the error to the calling function if the error happens immediately (as in the browser is offline)
-        (err) => console.error('Error fetching from server', err));
+        }, (error) => {
+          // Need this to propagate the error to the calling function if the error happens immediately (as in the browser is offline)
+          console.error('Error fetching from server', error);
+          subscriber.next(error);
+        });
 
-      const memValue: T = <T>this.memMap[key];
-      if (memValue) {
-        // return immediately
-        return Observable.of(_.cloneDeep(memValue));
-      } else {
-        // will have to wait
-        return loadedOb;
       }
-    }
+
+    });
   }
 
 }
